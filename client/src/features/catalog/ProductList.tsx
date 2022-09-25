06b15827abd6881ed10374254839865d866bbaba
Julio Legrec <julio.legrec@codeworldwide.com>
2022-09-25 20:56:45 +0400

    End of section 4

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function ProductList({ products }: Props) {
 	return (
-		<Grid container spacing={4}>
+		<Grid container spacing={4} sx={{ mb: 4 }}>
 			{products.map((product) => (
 				<Grid item xs={3} key={product.id}>
 					<ProductCard product={product} />
